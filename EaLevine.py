cb3630bca1a15c87358c9ee9d827f0991cac4e7d
Luis Angel <l_angel_1771@hotmail.com>
2023-01-05 08:59:24 -0300

    Continuamos

--- EaLevine.py
+++ EaLevine.py
@@ -3,49 +3,71 @@ import matplotlib.pyplot as plt
 import glob
 import csv
 
-a = glob.glob('./*K.txt')
-k = np.char.replace(np.array(a), 'K.txt', '')
-l = np.char.replace(k, './', '')
-m = sorted(l.astype(float), key=float)
+def ActivationEnergyLevine():
+    a = glob.glob('./*K.txt')
+    k = np.char.replace(np.array(a), 'K.txt', '')
+    l = np.char.replace(k, './', '')
+    m = sorted(l.astype(float), key=float)
 
-# Carregando dados dos txt, usar listas é melhor para isso
+    # Carregando dados dos txt, usar listas é melhor para isso
 
-novo02 = []
-for i in range(len(a)):
-    novo = []
-    with open(a[i], 'r') as f:
-        lol = csv.reader(f)
-        for row in lol:
-            novo.append(row)
-    lol = novo + [l[i]]
-    novo02.append(lol)
+    novo02 = []
+    for i in range(len(a)):
+        novo = []
+        with open(a[i], 'r') as f:
+            lol = csv.reader(f)
+            for row in lol:
+                novo.append(row)
+        lol = novo + [l[i]]
+        novo02.append(lol)
 
-# Uma vez carregado pasamos para numpy array:
-matrix = np.reshape(np.array(novo02, dtype=object), (len(a), 2))
+    # Uma vez carregado pasamos para numpy array:
+    matrix = np.reshape(np.array(novo02, dtype=object), (len(a), 2))
 
-# Testamos 'matrix02 = np.reshape(matrix[1, 0], (501, 5))', estamos procurando
-# um paso de voltagem de 0.01 V, Agora introducimos um for para pegarmos I-d para esse paso:
+    # Testamos 'matrix02 = np.reshape(matrix[1, 0], (501, 5))', estamos procurando
+    # um paso de voltagem de 0.01 V, Agora introducimos um for para pegarmos I-d para esse paso:
 
-array = []
-for i in range(len(a)):
-    matrix02 = np.reshape(matrix[i, 0], (501, 5))
-    for j in range(101):
-        c = (5*j)
-        d = matrix02[c]
-        e = np.r_[d, [j, l[i]]]
-        array.append(e)
+    array = []
+    for i in range(len(a)):
+        matrix02 = np.reshape(matrix[i, 0], (501, 5))
+        for j in range(101):
+            c = (5*j)
+            d = matrix02[c]
+            e = np.r_[d, [j, l[i]]]
+            array.append(e)
 
-# Reajeitamos, em 21 txt, 101 pontos a ler, 7 columnas:
+    # Reajeitamos, em 21 txt, 101 pontos a ler, 7 columnas:
 
-matrix03 = np.reshape(np.array(array), (21, 101, 7)).astype(float)
+    matrix03 = np.reshape(np.array(array), (21, 101, 7)).astype(float)
 
-graficos = []
-for i in range(101):
-    ordenando = matrix03[:, i][matrix03[:, i][:, 6].argsort()]
-    ordenando02 = np.c_[ordenando, 1/ordenando[:, 6]]
-    graficos.append(ordenando02)
+    graficos = []
+    for i in range(101):
+        ordenando = matrix03[:, i][matrix03[:, i][:, 6].argsort()]
+        ordenando02 = np.c_[ordenando, 1/ordenando[:, 6]]
+        graficos.append(ordenando02)
 
-print(matrix03[3][0])
+    # print(matrix03[3][0])
+
+
+def myplot():
+    bis = np.sort(glob.glob('./*.txt'))
+    arr = np.char.replace(np.array(bis), '.txt', '')
+    arr02 = np.char.replace(arr, './', '')
+    # print(arr02)
+    for i in range(len(bis)):
+        c = np.genfromtxt(bis[i], dtype=float, delimiter=',')
+        d = np.reshape(c, (501, 5))
+        e = np.c_[d, np.abs(d[:, 1])]
+        plt.title('Amostra1395 n\ Mesa05Terra07')
+        plt.scatter(e[:, 0], e[:, 5])
+        plt.yscale('log')
+        plt.legend(arr02, prop={'size': 8})
+        plt.grid(b=True, which='minor', color='b', linestyle='--')
+        plt.grid(b=True, which='major', color='b', linestyle='-')
+    plt.show()
+    
+print(myplot())
+
 
 #  fig, ax = plt.subplots()
 # ax.scatter(graficos[10][:, 7], graficos[10][:, 1])
